7717ca247aa81f6058302814e07ff96df0ff3ac4
khanhnehe <nguyenngockhanhvthg@gmail.com>
2024-02-23 09:03:13 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+// app.js
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/contacts", contactsRouter);
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+    res.json({ message: "Welcome to the contact book application." });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 module.exports = app;
